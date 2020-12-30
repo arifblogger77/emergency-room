24f0624fa626922ae08eb9bbc44d2b5568d9fdba
Arif Nur Rochman <arifblogger77@gmail.com>
2020-12-31 00:33:13 +0700

    add menu

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,22 @@
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ __('Master') }}
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('welcome') }}">
+                {{ __('Person') }}
+            </a>
+            <a class="dropdown-item" href="{{ route('welcome') }}">
+                {{ __('Email') }}
+            </a>
+            <a class="dropdown-item" href="{{ route('welcome') }}">
+                {{ __('Phone No') }}
+            </a>
+            <a class="dropdown-item" href="{{ route('welcome') }}">
+                {{ __('Phone Address') }}
+            </a>
+        </div>
+
+    </li>
